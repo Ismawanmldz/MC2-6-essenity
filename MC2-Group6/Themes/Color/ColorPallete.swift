43554df1ee2585d190c038ac34f663838aa56c0c
elvinajc <elvina14jc@gmail.com>
2022-06-27 11:15:27 +0700

    Timer + setting done, tab bar done

---
+++ MC2-Group6/Themes/Color/ColorPallete.swift
@@ -0,0 +1,17 @@
+//
+//  ColorPallete.swift
+//  MC2-Group6
+//
+//  Created by Elvina Jacia on 27/06/22.
+//
+
+
+import UIKit
+
+extension UIColor {
+    static var darkBlue = UIColor(red: 0.00, green: 0.36, blue: 0.57, alpha: 1.00)
+    static var systemGray5 = UIColor(red: 0.95, green: 0.96, blue: 0.97, alpha: 1.00)
+    static var orangeFocus = UIColor(red: 1.00, green: 0.64, blue: 0.00, alpha: 1.00)
+    static var blueBreak = UIColor(red: 0.40, green: 0.83, blue: 1.00, alpha: 1.00)
+    static var softBlue = UIColor(red: 0.29, green: 0.54, blue: 0.69, alpha: 1.00)
+}
