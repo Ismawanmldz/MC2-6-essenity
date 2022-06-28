6b5f057ae493c0de4f37c93d498b366c6fc83c07
gonszalito <andryanajona@gmail.com>
2022-06-28 20:14:11 +0700

    Added Reminder

---
+++ MC2-Group6/Feature/TaskDetails/UpdatinTime.swift
@@ -0,0 +1,42 @@
+////
+////  File.swift
+////  MC2-Group6
+////
+////  Created by Jonathan Andryana on 28/06/22.
+////
+//
+//
+//import UIKit
+//import CoreData
+//import Foundation
+//
+//class UpdatingTime {
+//    let calendar = Calendar.current
+//    let now = Date()
+//    
+//    let taskRepository = TaskRepository.shared
+//
+//    let timer = DispatchQueue.main.asyncAfter(deadline: <#T##DispatchTime#>, execute: <#T##() -> Void#>)
+//    let
+//    
+//    if now < Date.addingTimeInterval(10){
+//        
+//        dis
+//        
+//        let taskRepository = se
+//        
+//        let defaults = UserDefaults.standard
+//        let timerKey = "timerKey"
+//        
+//        defaults.set([])
+//        print(Date())
+//        
+//        let timerHolder = [Timer]
+//        let timer = Timer(fireAt: <#T##Date#>, interval: <#T##TimeInterval#>, target: <#T##Any#>, selector: <#T##Selector#>, userInfo: <#T##Any?#>, repeats: <#T##Bool#>)
+//        Timer.
+//        timer.in
+//    }
+//
+//
+//}
+//
